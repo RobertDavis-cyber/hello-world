e4c6eb178cbd4a77401c226e2b308a459f49cd08
Robert Davis <robertcd@hawaii.edu>
2022-10-04 18:07:15 -1000

    dynamic page with JavaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+function rateMyPage(){
+    let rating = prompt(On a scale of 1-5, how embarrassed should I be of this page?"
+    for(let i =0; i < rating; I++){
+        document.write('img src= "dummies.gif" alt='dummies'/>');
+    }
+    
+}
