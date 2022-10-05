704984b927a6ac3f26cb134d701ddae01e5f0dfe
Robert Davis <robertcd@hawaii.edu>
2022-10-04 18:34:50 -1000

    added functions js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+'use strict'
+
+function greetTheCustomer() {
+    let name = prompt('What is your name?');
+
+    return document.write(name);
+}
+
+function dayOfMonth() {
+    let day = prompt(What number day is it? (1-31)');
+    let message = '';
+    
+    if (day <= 1 {
+        message = 'Wow first of the month!;
+    } else if (day <= 31) {
+        message = 'This month is going by fast!';
+    } else if (day < 24) {
+        message = "Wow the days are going quick!";
+    } else {
+        message = "That hour does not exist!";
+    }
+
+    return message;
+}
+
 function rateMyPage(){
     let rating = prompt(On a scale of 1-5, how embarrassed should I be of this page?"
     for(let i =0; i < rating; I++){
