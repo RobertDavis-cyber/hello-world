9f607e7344503cbf6e95d8ec34c19f4f6d7d6134
Robert Davis <robertcd@hawaii.edu>
2022-10-06 14:30:46 -1000

    added js

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 'use strict'
 
-function greetTheCustomer() {
-    let name = prompt('What is your name?');
-
-    return document.write(name);
-}
-
-function dayOfMonth() {
-    let day = prompt(What number day is it? (1-31)');
-    let message = '';
-    
-    if (day <= 1 {
-        message = 'Wow first of the month!;
-    } else if (day <= 31) {
-        message = 'This month is going by fast!';
-    } else if (day < 24) {
-        message = "Wow the days are going quick!";
-    } else {
-        message = "That hour does not exist!";
+function checkPassword() {
+    var password = document.getElementByID("passwordBox");
+    var passwordText = password.value;
+    if(passwordText == "Seattle102") {
+        return true;
     }
-
-    return message;
+    alert("Access denied!  Incorrect password!");
+    return false;   
 }
 
-function rateMyPage(){
-    let rating = prompt(On a scale of 1-5, how embarrassed should I be of this page?"
-    for(let i =0; i < rating; I++){
-        document.write('img src= "dummies.gif" alt='dummies'/>');
+  function sayHello(name) {
+    alert("Hello, " + name);
+}
+sayHello("Rad Student!");
+}
+
+
     }
     
 }
